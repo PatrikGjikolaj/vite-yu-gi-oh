57c063f520f4508302173003c009be37d0053d33
PatrikGjikolaj <123626448+PatrikGjikolaj@users.noreply.github.com>
2023-05-23 17:27:28 +0200

    fix: bordo delle card

--- src/components/AppCards.vue
+++ src/components/AppCards.vue
@@ -19,7 +19,7 @@ export default {
             </div>
         </div>
         <div class="row gap-3 justify-content-between">
-            <div class="col-2 bg_orange text-center d-flex flex-column justify-content-between"
+            <div class="col-2 bg_orange text-center d-flex flex-column justify-content-between p-0"
                 v-for="(card, c) in store.cards">
                 <img :src="card.card_images[0].image_url" alt="" class="img-fluid">
                 <h5 class="text-white">{{ card.name }}</h5>
